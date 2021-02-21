a5166a69b20df21d3038f755200c3150ad2d7194
joachim <Joachim@johnsons.nu>
2021-02-21 15:38:20 +0100

    Old database templates removed
    Old database templates removed

--- Consid database Template/dbo/Tables/Companies.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Companies] (
-    [Id]                 UNIQUEIDENTIFIER NOT NULL,
-    [Name]               NVARCHAR (255)   NOT NULL,
-    [OrganizationNumber] INT              NOT NULL,
-    [Notes]              NVARCHAR (255)   NULL,
-    CONSTRAINT [PK_Companies] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
